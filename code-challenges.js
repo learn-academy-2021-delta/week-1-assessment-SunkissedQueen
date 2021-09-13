c8be5d9a8df57223ceeffb2129ca7f394048116c
SunkissedQueen <baxtercm@outlook.com>
2021-09-12 20:42:55 -0700

    Charlean completed wk 1 assessment

--- code-challenges.js
+++ code-challenges.js
@@ -6,30 +6,87 @@
 // If you get stuck, please leave comments to help us understand your thought process
 
 // --------------------1) Create a statement that determines if a temperature is below boiling point, at boiling point, or above boiling point. Boiling point is 212 degrees Fahrenheit.
+//This appears to be an evaluation. I would assign a number value to a variable then make a series of conditional statements using the if/elseif/else functions. These statements would compare the variable against the number 212 and log a sentence using string interpolation that includes that value and a result of whether it is below, above, or at boiling point.
 
 // Create the code and test each of the variables provided. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
+/*
+var temp = 35;
+if (temp === 212) {
+  console.log(temp + " is at boiling point")
+} else if(temp < 212) {
+  console.log(temp + " is below boiling point")
+} else if(temp ==== "string") {
+console.log("Please type a number.")
+} else {
+  console.log(temp + " is above boiling point")
+};
+*/
 
 var temp = 35
 // var temp = 350
 // var temp = 212
 
 
-
 // --------------------2) Create the code that will combine two arrays and return the length using the variables provided below. Expected output: 10
-
+//Concat and Length properties. I would assign an array list to two separate variables using the square brackets. (Thank you for providing it.) Next I would make a variable that would concat those arrays. Then I would perform a length propety command.
 var myNumbers1 = [3, 7, 0, 36, -9]
 var myNumbers2 = [8, -7, 22, 9, 13]
 
-
+/*
+var comboNumbers = myNumbers1.concat(myNumbers2)
+console.log(comboNumbers.length)
+*/
 
 // --------------------3) Create the code that will reverse the letters of a string using the test variables provided below. Expected output: "eilrahc", "atled"
+//Turning strings into arrays then back to a string. In order to use the reverse function, I will use the split function to turn the string into an array. For the list to populate as separate words, I will use a quotation marks and a space when assigning the split property. Then join property with a space and comma to produce an output with the string separated by a comma.
 
 var myString1 = "charlie"
 var myString2 = "delta"
 
+// So this question really gave my everything a sweat. I was going to remove the other attempts, but it was an interesting journey. So get ready for the fun. I wasn't sure if a string or an array expected for the output. So I worked hard to produce both.
+// First failed attempt console.log(myString1.split("").reverse().join("").concat(myString2.split("").reverse().join("")))
+//I get the output as eilrahcatled.
 
+// Testing each portion of my code because the split(" ") was undoing my reverse.
+//var myString1 = "charlie"
+//var myString2 = "delta"
+//console.log((myString1.split(" ").concat(myString2.split(" "))).join(", "))
+//gives me an output of charlie, delta
+
+//console.log(myString1.split("").reverse().join(""))
+//output of eilrahc
+
+// console.log(myString1.split(" ").reverse().join("").concat(myString2.split(" ").reverse().join("")))
+// output of charliedelta
+
+// My brain is tired attempt. I know I want to split to make an array. Reverse the arrays, then join them. But it is not working.
+
+// console.log(myString1.split(" ").reverse( ).concat(myString2.split(" ").reverse( )).join(", "))
+// output as a string without quotations and not in reverse--- charlie, delta
+
+// console.log(myString1.split("").reverse().join("")); console.log(myString2.split("").reverse().join(""))
+// output on separate lines--- eilrahc
+//  atled
+
+// This was a desperate move.
+// var myString3 = " ,"
+// console.log(myString1.split("").reverse().join("") + myString3.split("").reverse().join("") + myString2.split("").reverse().join(""))
+//output without quotations --- eilrahc, atled
+
+// Of course, I had to keep trying. It's not 8:59am on Monday yet.
+// var myString1 = "charlie"
+// var myString2 = "delta"
+// console.log(myString1.split("").reverse().join("").concat(", ", myString2.split("").reverse().join("")))
+// output as a string without quotations     eilrahc, atled
+
+// The only way I could get quotation is in an array...wait a minute, did you want quotations...well here is the code anyway.
+// var myString1 = "charlie"
+// var myString2 = "delta"
+// console.log(myString1.split("").reverse().join("").concat(" ", myString2.split("").reverse().join("")).split(" "))
+// output   ['eilrahc', 'atled']
 
 // --------------------4) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 15, 12
+// This appears to be an evaluation. I will make a series of conditional statements using the if/elseif/else functions. These statements would compare the variable against the each other and log the difference. My catchall will request that the user enter a number value in case letters were entered.
 
 var number1 = 42
 var number2 = 27
@@ -37,6 +94,13 @@ var number2 = 27
 // var number1 = 7
 // var number2 = 19
 
+// if (number1 === number2) {
+//   console.log("Please enter two different numbers.")
+// } else if(number1 > number2) {
+//   console.log(number1 - number2)
+// } else{
+//   console.log(number2 - number1)
+// };
 
 
 // --------------------5) Copy/paste your code from #4. Refactor your code to also accept non-number edge cases. Inform your user if the variable is not a number using the two sets of test variables provided below. Expected output: "Your input is not a number", 3
@@ -46,3 +110,14 @@ var numberRefactor2 = "hello"
 
 // var numberRefactor1 = 27
 // var numberRefactor2 = 24
+// if (typeof numberRefactor1 === "string") {
+//   console.log("Your input is not a number")
+// } else if(typeof numberRefactor2 === "string") {
+//     console.log("Your input is not a number")
+//   } else if(numberRefactor1 === numberRefactor2) {
+//   console.log("Please enter two different numbers.")
+// } else if(numberRefactor1 > numberRefactor2) {
+//   console.log(numberRefactor1 - numberRefactor2)
+// } else{
+//   console.log(numberRefactor2 - numberRefactor1)
+// };
--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 var cohort = "Delta 2021"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: 10
+// b) Verify and explain: Answer correct because the length property counts in numerical order starting at 1 and counts a space as a character.
 
 
 // --------------------2) What will this log?
@@ -19,8 +19,8 @@ var cohort = "Delta 2021"
 var greeting = "Hello World!"
 // console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: l
+// b) Verify and explain: Answer correct because there is a zero index when locating the place of a character...so the count starts at 0.
 
 
 // --------------------3) What will this log?
@@ -29,8 +29,8 @@ var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Ruby
+// b) Verify and explain: Answer correct because there is a zero index when locating the place of a character...so the count starts at 0. Each language is counted as a single location because it is in an array.
 
 
 // --------------------4) What will this log?
@@ -38,14 +38,14 @@ var index = 1
 var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: SATURDAY, SUNDAY
+// b) Verify and explain: "weekendDays.toUpperCase is not a function." (I copied the stuff in quotations from the console. All these other words are mine.) I am assuming there is some extra steps needed to capitalize an array. Join function to make a string and then use .toUpperCase(). My head hurts too much from trying to reverse that other array.
 
 
 // --------------------5) What will this log?
 
 var dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Torn between 6, 6, 7 and 19 because of all of these would be considered strings
+// b) Verify and explain: "number" I was so confused, but I see that typeof is an operator...so it wants to determine what follows it. In this case, the next thing is the actual number of characters that the length property states as 3 because it's a list in an array. Wow! My initial guess was in a different state. Anyway...type of 3 will output number.
